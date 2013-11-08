90f86a7eb320b93a92bf9776d994bba983a8c21a
Gabe Hayes <gabriel.hayes@gmail.com>
2013-11-08 03:04:05 -0800

    linter no longer supports globals parameter - also updates the pattern variable to support an array

--- test/plugin_test.js
+++ test/plugin_test.js
@@ -3,11 +3,10 @@ describe('Plugin', function() {
 
   beforeEach(function() {
     plugin = new Plugin({
-      paths: {watch: 'app'},
+      paths: {watch: ['app']},
       plugins: {
         coffeelint: {
-          options: {no_trailing_semicolons: { level: "ignore"} },
-          globals: {stuff: true}
+          options: {no_trailing_semicolons: { level: "ignore"} }
         }
       }
     });
