0b8d1a80e4afd837d59665a97f23d486b72762ea
Gosha Arinich <me@goshakkk.name>
2016-01-28 21:09:56 +0300

    Update interface to 2.0

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
+const expect = require('chai').expect;
+const Plugin = require('.');
+
 describe('Plugin', function() {
   var plugin;
 
@@ -22,16 +25,13 @@ describe('Plugin', function() {
   it('should lint correctly', function(done) {
     var content = 'a = 228\nb = () ->\n  console.log a'
 
-    plugin.lint(content, 'file.coffee', function(error) {
-      expect(error).not.to.be.ok;
-      done();
-    });
+    plugin.lint({data: content, path: 'file.coffee'}).then(() => done(), error => expect(error).not.to.be.ok);
   });
 
   it('should give correct errors', function(done) {
     var content = 'b = () ->\n\t\t   a=10'
 
-    plugin.lint(content, 'file.coffee', function(error) {
+    plugin.lint({data: content, path: 'file.coffee'}).then(null, error => {
       expect(error).to.contain('error: indentation at line 2. Expected 2 got 5\nerror: no_tabs at line 2.');
       done();
     });
@@ -40,10 +40,7 @@ describe('Plugin', function() {
   it('should read configs global options list', function(done) {
     var content = 'alert("end of line");'
 
-    plugin.lint(content, 'file.coffee', function(error) {
-      expect(error).not.to.be.ok;
-      done();
-    });
+    plugin.lint({data: content, path: 'file.coffee'}).then(() => done(), error => expect(error).not.to.be.ok);
   });
 
   it('should read coffeelint.json', function(done) {
@@ -57,7 +54,7 @@ describe('Plugin', function() {
       }
     });
 
-    plugin.lint(content, 'file.coffee', function(error) {
+    plugin.lint({data: content, path: 'file.coffee'}).then(null, error => {
       expect(error).to.contain('error: no_plusplus at line 1. found \'++\'');
       done();
     });
